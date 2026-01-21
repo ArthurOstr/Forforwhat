160240b5711e8a8d47de7fdb5f2685087c80db5f
ArthurOstr <automata4231@gmail.com>
2026-01-21 21:08:03 +0300

    Fix database connection and downgrade psycopg2

--- database.py
+++ database.py
@@ -1,17 +1,11 @@
 import os
-import psycopg
+import psycopg2
 from dotenv import load_dotenv
 load_dotenv()
 
 def get_db_connection():
     try:
-        conn = psycopg.connect(
-            dbname=os.getenv('DB_NAME'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            host=os.getenv('DB_HOST'),
-            port=os.getenv('DB_PORT')
-        )
+        conn = psycopg2.connect(os.getenv("DATABASE_URL"))
         return conn
     except Exception as e:
         print(f"Error connecting to PostgreSQL: {e}")
